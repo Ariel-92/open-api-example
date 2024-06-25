ca37f1cd134026498a3b932a54cc4486421cea33
sylee <sylee@plateer.com>
2024-06-25 11:26:30 +0900

    refact product API

--- src/main/java/com/rockercats/open_api/service/ProductService.java
+++ src/main/java/com/rockercats/open_api/service/ProductService.java
@@ -3,19 +3,29 @@ package com.rockercats.open_api.service;
 import com.rockercats.open_api.model.product.ProductDetailRequest;
 import com.rockercats.open_api.model.product.ProductDetailResponse;
 import com.rockercats.open_api.repository.OpenApiExamProductMapper;
+import io.github.bucket4j.Bucket;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@Slf4j
 @RequiredArgsConstructor
+@Service
 public class ProductService {
     private final OpenApiExamProductMapper openApiExamProductMapper;
+    private final BucketService bucketService;
 
-    public List<ProductDetailResponse> getProducts(ProductDetailRequest productDetailRequest) {
-        return openApiExamProductMapper.selectProductList(productDetailRequest);
+    public ResponseEntity<List<ProductDetailResponse>> getProducts(ProductDetailRequest productDetailRequest, HttpServletRequest request) {
+        Bucket bucket = bucketService.resolveBucket(request);
+
+        if (!bucket.tryConsume(1)) {
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+
+        List<ProductDetailResponse> productDetailResponseList = openApiExamProductMapper.selectProductList(productDetailRequest);
+        return ResponseEntity.ok().body(productDetailResponseList);
     }
 }
